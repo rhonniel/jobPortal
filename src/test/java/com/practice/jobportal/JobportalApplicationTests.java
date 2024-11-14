c5efc9968a6a7e6ccfdbb910fb37596406ed96bc
rhonniel <rhonniel777@gmail.com>
2024-11-13 20:22:30 -0400

    first commit

---
+++ src/test/java/com/practice/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,18 @@
+package com.practice.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
